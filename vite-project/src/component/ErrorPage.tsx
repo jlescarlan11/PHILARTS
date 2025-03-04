9e2c3ed628ed46a97489d8e542f9a4a0dcd63ee7
john lester escarlan <jlescarlan11@gmail.com>
2025-03-04 23:59:27 +0800

    added margin bottom

--- vite-project/src/component/ErrorPage.tsx
+++ vite-project/src/component/ErrorPage.tsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
-    <div className="text-center text[var(--color-secondary)]">
+    <div className="text-center text[var(--color-secondary)] flex flex-col gap-4">
       It is not available in the meantime. Please come back later.
+      <button
+        onClick={() => navigate("/")}
+        className="text-[var(--color-primary)] bg-[var(--color-accent)] py-2 px-4 rounded hover:bg-[var(--color-accent-90)] transition duration-200 focus:outline-none focus:ring-2 focus:ring-[var(--color-secondary)]"
+      >
+        Go Home
+      </button>
     </div>
   );
 };
